bac90382da7f5b9196998f7e58eb933d75e4dd8b
Md. Hasibul Islam <45915540+hasib999@users.noreply.github.com>
2023-06-17 16:55:12 +0600

    useReducer Hook type

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
-import NewUserS from './components/NewUserS';
+import Counter from './components/Counter';
 
 
 function App() {
   
   return (
     <div className="App">
-      <NewUserS/>
+      <Counter/>
     </div>
   );
 }
